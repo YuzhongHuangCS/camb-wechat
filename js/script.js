3febf09b7da90291befbacb99e5ef8152886f861
PillowSky <ihyzi@163.com>
2014-03-03 21:47:08 +0800

    rewrite script.js

--- js/script.js
+++ js/script.js
@@ -22,22 +22,21 @@ function loadList(id){
 	var query = "php/list.php?listID=" + id;
 	$.getJSON(query,function(result){
 		var content,i;
-		content = "<div class='ui-grid-a ui-responsive'>";
+		content =	'<div class="ui-grid-a ui-responsive">';
 		for (i=0; i<result.length; i++) {
 			if (i % 2) {
-				content += "<div class='ui-block-b'>";
+			content += '<div class="ui-block-b">';
 			} else {
-				content += "<div class='ui-block-a'>";
+			content += '<div class="ui-block-a">';
 			}
-			content += "<div class='arc ui-shadow ui-corner-all'><p class='title'>";
-			content += result[i].title;
-			content += "</p><div class='box'><img src='";
-			content += result[i].img;
-			content += "'/></div><p class='more'>";
-			content += result[i].more;
-			content += "</p></div></div>";
+			content +=		'<div class="arc ui-shadow ui-corner-all">';
+			content +=			'<p class="title">'; content += result[i].title; content += '</p>';
+			content +=			'<div class="box"><img src="'; content += result[i].img; content += '"/>'; content += '</div>';
+			content += 			'<p class="more">'; content += result[i].more; content += '</p>';
+			content += 		'</div>';
+			content += '</div>';
 		};
-		content += 	"</div></div>";
+		content +=	'</div>';
 		$("#list").html(content);
 	});
 };
